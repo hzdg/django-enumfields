492a3438694f267d95736657c1477079ba6b1b59
Mikhail Turilin <mturilin@gmail.com>
2014-05-08 11:03:37 -0700

    Added enum param could be string (to support south)

--- enumfields/fields.py
+++ enumfields/fields.py
@@ -1,16 +1,23 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from enum import Enum
 import six
 
 
 class EnumFieldMixin(six.with_metaclass(models.SubfieldBase)):
-    def __init__(self, enum, choices=None, max_length=10, **options):
-        self.enum = enum
+    def __init__(self, enum,  choices=None, max_length=10, **options):
+        if isinstance(enum, basestring):
+            module_name, class_name = enum.rsplit('.', 1)
+            module = __import__(module_name, globals(), locals(), [class_name])
+            self.enum = getattr(module, class_name)
+        else:
+            self.enum = enum
+
         if not choices:
             try:
-                choices = enum.choices()
+                choices = self.enum.choices()
             except AttributeError:
-                choices = [(m.value, getattr(m, 'label', m.name)) for m in enum]
+                choices = [(m.value, getattr(m, 'label', m.name)) for m in self.enum]
         super(EnumFieldMixin, self).__init__(
             choices=choices, max_length=max_length, **options)
 
@@ -33,31 +40,34 @@ class EnumField(EnumFieldMixin, models.CharField):
 
 
 class EnumIntegerField(EnumFieldMixin, models.IntegerField):
-    pass
-
-
+    def get_prep_value(self, value):
+        if value is None:
+            return None
+        elif isinstance(value, Enum):
+            return value.value
+        else:
+            return int(value)
 
 
 # South compatibility stuff
 
 def converter_func(enum_class):
-    return "__import__('%(module)s', globals(), locals(), ['%(class)s']).%(class)s" % \
-            {"module":enum_class.__module__,
-             "class": enum_class.__name__}
+    return "'%s.%s'" % (enum_class.__module__, enum_class.__name__)
 
 
 rules = [
-  (
-    [EnumFieldMixin],
-    [],
-    {
-        "enum": ["enum", {'is_django_function':True, "converter": converter_func}],
-    },
-  )
+    (
+        [EnumFieldMixin],
+        [],
+        {
+            "enum": ["enum", {'is_django_function': True, "converter": converter_func}],
+        },
+    )
 ]
 
 try:
     from south.modelsinspector import add_introspection_rules
+
     add_introspection_rules(rules, ["^enumfields\.fields"])
 except ImportError:
     pass
