hzdg/django-enumfields

commit f5630f7b7d0caf7479c90b29b8ebf05a476f6b44

Use enum member `.value` (instead of `str()`) in fields.
This seems like the sane thing to do, especially in presence of things
that may dynamically modify the `str()` of an object at run time. (Looking
at you, lazy translation objects.)

enumfields/fields.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from enum import Enum
 import six
-from django.db.models.fields import NOT_PROVIDED
+from django.db.models.fields import NOT_PROVIDED, BLANK_CHOICE_DASH
 
 try:
     from django.utils.module_loading import import_string
@@ -66,6 +66,15 @@ class EnumFieldMixin(six.with_metaclass(models.SubfieldBase)):
                 kwargs["default"] = kwargs["default"].value
 
         return name, path, args, kwargs
+
+    def get_choices(self, include_blank=True, blank_choice=BLANK_CHOICE_DASH):
+        # Force enum fields' options to use the `value` of the enumeration
+        # member as the `value` of SelectFields and similar.
+        return [
+            (i.value if i else i, display)
+            for (i, display)
+            in super(EnumFieldMixin, self).get_choices(include_blank, blank_choice)
+        ]
 
 
 class EnumField(EnumFieldMixin, models.CharField):

tests/test_enums.py
@@ -113,18 +113,25 @@ def superuser_client(client, superuser):
 def test_model_admin(superuser_client):
     url = reverse("admin:tests_mymodel_add")
     secret_uuid = str(uuid.uuid4())
-    response = superuser_client.post(url, follow=True, data={
-        'color': 'Color.RED',
-        'taste': 'Taste.UMAMI',
-        'taste_int': 'Taste.SWEET',
+    post_data = {
+        'color': MyModel.Color.RED.value,
+        'taste': MyModel.Taste.UMAMI.value,
+        'taste_int': MyModel.Taste.SWEET.value,
         'random_code': secret_uuid
-    })
+    }
+    response = superuser_client.post(url, follow=True, data=post_data)
     response.render()
     text = response.content
 
     assert b"This field is required" not in text
     assert b"Select a valid choice" not in text
-    assert MyModel.objects.filter(random_code=secret_uuid).exists(), "Object wasn't created in the database"
+    try:
+        inst = MyModel.objects.get(random_code=secret_uuid)
+    except MyModel.DoesNotExist:
+        assert False, "Object wasn't created in the database"
+    assert inst.color == MyModel.Color.RED, "Redness not assured"
+    assert inst.taste == MyModel.Taste.UMAMI, "Umami not there"
+    assert inst.taste_int == MyModel.Taste.SWEET, "Not sweet enough"
 
 
 def test_django_admin_lookup_value_for_integer_enum_field():

commit e493237165bbba41dd90606b81827be03f074d27

Fix form field regression introduced in f5630f7b

enumfields/fields.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from enum import Enum
+from .forms import EnumChoiceField
 import six
 from django.db.models.fields import NOT_PROVIDED, BLANK_CHOICE_DASH
 
@@ -75,6 +76,11 @@ class EnumFieldMixin(six.with_metaclass(models.SubfieldBase)):
             for (i, display)
             in super(EnumFieldMixin, self).get_choices(include_blank, blank_choice)
         ]
+
+    def formfield(self, form_class=None, choices_form_class=None, **kwargs):
+        if not choices_form_class:
+            choices_form_class = EnumChoiceField
+        return super(EnumFieldMixin, self).formfield(form_class=form_class, choices_form_class=choices_form_class, **kwargs)
 
 
 class EnumField(EnumFieldMixin, models.CharField):

enumfields/forms.py
@@ -1,0 +1,29 @@
+# -- encoding: UTF-8 --
+from django.forms import TypedChoiceField
+from django.forms.fields import TypedMultipleChoiceField
+from django.utils.encoding import force_text
+
+__all__ = ["EnumChoiceField", "EnumMultipleChoiceField"]
+
+class EnumChoiceFieldMixin(object):
+    def prepare_value(self, value):
+        # Widgets expect to get strings as values.
+
+        if value is None:
+            return ''
+        if hasattr(value, "value"):
+            value = value.value
+        return force_text(value)
+
+    def valid_value(self, value):
+        if hasattr(value, "value"):  # Try validation using the enum value first.
+            if super(EnumChoiceFieldMixin, self).valid_value(value.value):
+                return True
+        return super(EnumChoiceFieldMixin, self).valid_value(value)
+
+
+class EnumChoiceField(EnumChoiceFieldMixin, TypedChoiceField):
+    pass
+
+class EnumMultipleChoiceField(EnumChoiceFieldMixin, TypedMultipleChoiceField):
+    pass

tests/test_form_fields.py
@@ -1,0 +1,24 @@
+# -- encoding: UTF-8 --
+
+from django.forms.models import modelform_factory
+import pytest
+from .models import MyModel
+import six
+
+
+def get_form(**kwargs):
+    instance = MyModel(color=MyModel.Color.RED)
+    FormClass = modelform_factory(MyModel, fields=("color",))
+    return FormClass(instance=instance, **kwargs)
+
+
+@pytest.mark.django_db
+def test_unbound_form_with_instance():
+    form = get_form()
+    assert 'value="r" selected="selected"' in six.text_type(form["color"])
+
+
+@pytest.mark.django_db
+def test_bound_form_with_instance():
+    form = get_form(data={"color": "g"})
+    assert 'value="g" selected="selected"' in six.text_type(form["color"])