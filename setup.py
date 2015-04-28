99bc67815176968292616efbc88df0480f3c87ce
Matthew Dapena-Tretter <m@tthewwithanm.com>
2015-04-28 11:14:40 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ except ImportError:
 
 setup(
     name='django-enumfields',
-    version='0.7.2',
+    version='0.7.3',
     author='HZDG',
     author_email='webmaster@hzdg.com',
     description='Real Python Enums for Django.',
