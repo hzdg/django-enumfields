6ab8bb133fe214b1144be5caf203c0fe70825c38
johnthagen <johnthagen@gmail.com>
2019-01-04 08:02:44 -0500

    Add support for Python 3.7 and Django 2.1

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
@@ -54,6 +55,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
     ],
     tests_require=[
